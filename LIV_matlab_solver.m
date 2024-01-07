02202fdae30c9aa93f28e6745a79ef4be429c823
Phillip Blakey <phil.blakey@mail.utoronto.ca>
2024-01-07 13:15:08 -0500

    LIV_RK45 working

--- LIV_matlab_solver.m
+++ LIV_matlab_solver.m
@@ -33,15 +33,18 @@ tau_s = 1e3*2.85e7*T/(N_c*1e6);
 I_th = e*N_th*V_a/tau_s;
 I = linspace(2*I_th,2*I_th,n_current);
  
-S_tol = 10;
+S_tol = 100;
 N_tol = N_g/1000;
 
 y0 = [0 N_th];
 tspan = [0,1e-05];
 
 [t,y] = ode45(@odefun, tspan, y0);
-
-plot(t,y(:,1))
+Pwr = nu/(2*L)*(1/(Rb*Rf))*y(:,1)*hbar*omega;
+figure()
+plot(t,Pwr)
+figure()
+plot(t,y(:,2))
 
 function dydt = odefun(t,y)
 xi = 0.2;
@@ -63,11 +66,11 @@ N_th = G_th/(xi*a) + N_g;
 N_c = 3.65e18;
 tau_s = 1e3*2.85e7*T/(N_c*1e6);
 
-I_th = 0.91*e*N_th*V_a/tau_s;
+I = 0.9*e*N_th*V_a/tau_s;
 
 dydt = zeros(2,1);
-dydt(1) = -(xi*a*(y(2) - N_g) -G_th)*y(1) + xi*a*y(2);
-dydt(2) = xi*a*(y(2) - N_g)*y(1)/V_a -y(2)/tau_s + I_th/(e*V_a);
+dydt(1) = (xi*a*(y(2) - N_g) -G_th)*y(1) + xi*a*y(2);
+dydt(2) = -xi*a*(y(2) - N_g)*y(1)/V_a -y(2)/tau_s + I/(e*V_a);
 end
 
 
